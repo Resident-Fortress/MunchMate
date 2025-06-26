dbf7679071dbeea8986777e5b6f7d9ba526f6c0f
Matthew Dudzinski <42253757+Resident-Fortress@users.noreply.github.com>
2025-06-26 14:03:49 -0500

    Initial Commit

---
+++ MunchMate/MunchMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchMateApp.swift
+//  MunchMate
+//
+//  Created by Matthew Dudzinski on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MunchMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
